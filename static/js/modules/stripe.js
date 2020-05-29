5993fb346ce398daa696444a650f596f34fa334b
Sam Pawson <samuel.pawson@gmail.com>
2020-05-29 17:07:09 +0100

    Added s3 hosting for static/media

--- static/js/modules/stripe.js
+++ static/js/modules/stripe.js
@@ -83,3 +83,4 @@ $(function() {
         return false;
     });
 })
+
--- trips/settings.py
+++ trips/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
+    'storages',
     'stripe',
     'home',
     'accounts',
@@ -140,13 +141,36 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+AWS_S3_OBJECT_PARAMETER = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT', #Allows static files to be cached
+    'CacheControl': 'max-age=94608000',
+}
+
+#AWS Storage information
+AWS_STORAGE_BUCKET_NAME = 'trips-ecommerce'
+AWS_S3_REGION_NAME = 'eu-west-1'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+#any files with static at the root will be stored on AWS s3
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE= 'custom_storages.StaticStorage'
+#STATICFILES_STORAGE= 'storages.backends.s3boto3.S3Boto3Storage'
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+#'storages.backends.s3boto3.S3Boto3Storage'
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
