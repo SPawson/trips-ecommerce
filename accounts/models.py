0f4163e36469216f0f9ca10e46a3446723f17cd8
Sam Pawson <samuel.pawson@gmail.com>
2020-06-10 20:31:42 +0100

    user meta class added to model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User as AuthUser
+from django.utils import timezone
 
-# Create your models here.
+#User model
+class User(AuthUser):
+    class Meta:
+        proxy = True
