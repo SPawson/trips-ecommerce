5c1408a83321ba63708aae58c7d87065ed715ffd
Sam Pawson <samuel.pawson@gmail.com>
2020-06-14 13:15:59 +0100

    Filters show/hide + 404 page added

--- static/js/pages/products.js
+++ static/js/pages/products.js
@@ -1,18 +1,28 @@
-//used to initialise the datepicker on the product listing
-$(function() {
-    $('.dates #datepicker').datepicker({
-        'format': 'yyyy-mm-dd',
-        'autoclose': true
-    });
-});
-
-
-$(document).on('load', function(){
-
-    
-
+$(document).on('load', function () {
 
 
 
 
 })
+
+$("#show-filters").click(function () {
+    let icon = '  <i class="fas fa-sort"></i>'
+    $("#filters").toggle();
+
+    if ($("#filters").is(":visible")){
+        console.log("Filter visible");
+        $("#show-filters").html("Hide Filters" + icon);
+    }
+    else{
+        
+        $("#show-filters").html("Show Filters" + icon);
+    }
+});
+
+//used to initialise the datepicker on the product listing
+$(function () {
+    $('.dates #datepicker').datepicker({
+        'format': 'yyyy-mm-dd',
+        'autoclose': true
+    });
+});
