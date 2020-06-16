c5d2440966728115d6876f4ab77c43c33c20340a
Sam Pawson <samuel.pawson@gmail.com>
2020-06-16 17:38:01 +0100

    User model changed to resolve travis

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User as AuthUser
+from django.contrib.auth.models import User
 from django.utils import timezone
 
 #User model
-class User(AuthUser):
-    class Meta:
-        proxy = True
